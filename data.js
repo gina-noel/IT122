637b8560a6b0636529f01c3555832f3d8edb2320
gina-noel <Gina.Noel@seattlecolleges.edu>
2022-04-29 19:04:53 -0700

    Week 4 - Quality Matters

--- data.js
+++ data.js
@@ -18,13 +18,42 @@ const getItem = (url) => {
         return pose.name === url;
     });
 }
-//
-// const getItem = (name) => {
-//     // search the poses array
-//     return poses.find((pose) => {
-//         return pose.name === name;
-//     });
-// }
-// console.log(getItem("Tree"));
 
-export { getAll, getItem };
+const addItem = (item) => {
+    const newItem = getItem(item.name);
+    let msg = '';
+    if (newItem) {
+        msg = item.name + ' pose already exists!'
+        // console.log("item exists");
+    } else {
+        poses.push(item);
+        msg = item.name + ' pose has been added!'
+        // console.log("item added");
+    }
+    return { result: msg };
+}
+
+const deleteItem = (item) => {
+    const newItem = getItem(item.name);
+    let msg = '';
+    if (newItem) {
+        const index = poses.indexOf(newItem);
+        item.slice(index, 1);
+        msg = item.name + ' pose was deleted!'
+        // console.log("item deleted");
+        // console.log(poses);
+
+    } else {
+        msg = item.name + ' pose was not found!'
+        // console.log("item not deleted");
+    }
+    return { result: msg };
+}
+
+// addItem({name: "TestingTesting", benefit: "123", ability: "123",symbol:"123" })
+// deleteItem({name: "TestingTesting", benefit: "123", ability: "123",symbol:"123" })
+// addItem({name: "Tree", benefit: "123", ability: "123",symbol:"123" })
+// deleteItem({name: "Tree", benefit: "balance", ability: "easy",symbol:"universe" })
+// console.log(poses);
+
+export { getAll, getItem, addItem, deleteItem };
---
+++ test/test.js
@@ -0,0 +1,35 @@
+import { expect } from 'chai';
+import * as data from "../data.js";
+
+describe("data.js module", () => {
+    it("returns requested getItem()", function() {
+        let result = data.getItem("Tree");
+        expect(result).to.deep.equal({name: "Tree", benefit: "balance", ability: "easy", symbol: "universe"});
+    });
+
+    it("returns no items from getItem()", function() {
+        let result = data.getItem("blah");
+        expect(result).to.be.undefined;
+    });
+
+    it("returns addItem result was added result", function() {
+        let result = data.addItem({name: "test", benefit: "test", ability: "test", symbol: "test"});
+        expect(result).to.deep.equal({result: "test pose has been added!"});
+    });
+
+    it("returns addItem already exists result", function() {
+        let theItem = data.addItem({name: "test", benefit: "test", ability: "test", symbol: "test"});
+        let result = data.addItem({name: "test", benefit: "test", ability: "test", symbol: "test"});
+        expect(result).to.deep.equal({result: "test pose already exists!"});
+    });
+
+    it("returns deleteItem is successful", function() {
+        let result = data.deleteItem({name: "Tree"});
+        expect(result).to.deep.equal({result: "Tree pose was deleted!"});
+    })
+
+    it("returns deleteItem was not found", function() {
+        let result = data.deleteItem({name: "test", benefit: "test", ability: "test", symbol: "test"});
+        expect(result).to.deep.equal({result: "test pose was not found!"});
+    })
+});
