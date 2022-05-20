a92b5d48ddbc097304e81521ea1fe110cc9b4f9c
gina-noel <82776027+gina-noel@users.noreply.github.com>
2022-05-20 13:20:24 -0700

    Update Poses.js
    exercise 6

--- models/Poses.js
+++ models/Poses.js
@@ -16,7 +16,7 @@ mongoose.connection.on('open', () => {
 // define data model as JSON key/value pairs
 // values indicate the data type of each key
 const posesSchema = new Schema({
-    title: { type: String, required: true },
+//     title: { type: String, required: true },
     name: String,
     benefit: String,
     ability: String,
